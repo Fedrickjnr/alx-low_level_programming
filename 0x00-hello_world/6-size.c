ec5e200a686dce67d710d5a98f7abc9f618f9b6d
Fedrickjnr <ojomicheal036@gmail.com>
2023-06-18 03:52:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - 6-size
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a long int: %i byte(s)\n", sizeof(float));
+	printf("size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
